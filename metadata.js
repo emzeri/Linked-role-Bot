fa4f940022799b1489437f9f77e6d697aca43adc
Emzeri <rheatiangco490@gmail.com>
2025-09-28 18:30:16 +0800

    Update metadata.js

---
+++ metadata.js
@@ -0,0 +1,37 @@
+const linkedRoles = [
+  {
+    key: "owner_linked",
+    name: "Owner Linked Role",
+    description: "Member is a part of B&A Team",
+    type: 7,
+    position: 1
+  },
+  {
+    key: "manager_linked",
+    name: "Manager Linked Role",
+    description: "Member is a part of B&A Team",
+    type: 7,
+    position: 2
+  },
+  {
+    key: "moderator_linked",
+    name: "Moderator Linked Role",
+    description: "Member is a part of B&A Team",
+    type: 7,
+    position: 3
+  },
+  {
+    key: "member_linked",
+    name: "Member Linked Role",
+    description: "Member is a part of B&A Team",
+    type: 7,
+    position: 0
+  }
+];
+
+const attribution = {
+  footerText: "Powered by B&A STAFF",
+  footerIcon: "https://cdn.discordapp.com/app-icons/YOUR_APP_ID/YOUR_ICON.png"
+};
+
+module.exports = { linkedRoles, attribution };
